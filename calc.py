278423ff96fd681239afae753a7f22a34b653836
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-09 00:20:43 +0530

    menu structure and input loop

--- calc.py
+++ calc.py
@@ -1,1 +1,15 @@
-print("hello")
+#calculator project
+def menu():
+    print('''CALCULATOR
+          1.ADDITION
+          2. SUBTRACTION
+          3. MULTIPLICATION
+          4. DIVISION
+          5.EXIT''')
+    n=int(input("Enter an option[1-5]:"))
+    if n == 1:
+        n1=int(input("How many numbers do u want to enter:"))
+        for i in range(n1):
+            num=int(input("THE NUMBER IS:"))
+            print(num)
+menu()
