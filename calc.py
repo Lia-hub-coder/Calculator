d7d59e7ab6098bcbee2978231323c796e5f34cfd
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-09 00:57:50 +0530

    all four operations working with divide-by-zero guard

--- calc.py
+++ calc.py
@@ -29,4 +29,19 @@ def menu():
             num=int(input(f"THE {i+1} NUMBER IS:"))
             res=res*num
         print(res)
+    elif n==4:
+        n1=int(input("How many numbers do u want to enter:"))
+        num=int(input("Please specify the initial number you want to subtract from:"))
+        for i in range(n1):
+            num1=int(input(f"THE {i+1} NUMBER IS:"))
+            if num1==0:
+                print("Cannot enter zero!")
+            else:
+                num=num/num1
+        print(num)
+    elif n==5:
+        print("Bye! Have a nice day!")
+    else:
+        print("Wrong input.")
+
 menu()
