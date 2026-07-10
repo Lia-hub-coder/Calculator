81a455dbc5a8885c8977aadba45ac423d4184412
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-10 16:31:15 +0530

    name of the text file changed

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 #calculator project
 def menu():
-    with open ("Calculator.txt","a") as c:
+    with open ("HISTORY.txt","a") as c:
         initial_contents='CALCULATOR\n1. ADDITION\n2. SUBTRACTION\n3. MULTIPLICATION\n4. DIVISION\n5. VIEW HISTORY\n6.EXIT'
         print(initial_contents)
         n=int(input("Enter an option[1-6]:"))
@@ -39,7 +39,7 @@ def menu():
             for i in range(n1):
                 num1=int(input(f"THE {i+1} NUMBER IS:"))
                 if num1==0:
-                    print("Cannot enter zero!")
+                    print("Cannot enter zero! Zero won't be counted.")
                 else:
                     res=res/num1
             print(res)
@@ -47,7 +47,7 @@ def menu():
         elif n==5:
             print("This is view history")
             c.write(f"{s} visited the history!\n")
-            with open ("Calculator.txt","r") as history:
+            with open ("HISTORY.txt","r") as history:
                 contents = history.read()
                 print(contents)
         elif n==6:
