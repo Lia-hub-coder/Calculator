7965e99afda68f2e25913073eb9b1fcd17231ed9
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-11 17:12:24 +0530

    add numbers list to history log and fix view-history flush

--- calc.py
+++ calc.py
@@ -12,51 +12,60 @@ def menu():
             try:
                 n=int(input("Enter an option[1-6]:"))
                 now=datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+                numbers=[]
                 if n == 1:
                     n1=int(input("How many numbers do u want to enter:"))
                     res=int(input("Please specify the initial number you want to ADD to(Another n (number you selected) numbers will be asked from you after this!):"))
+                    numbers.append(res)
                     for i in range(n1):
                         num=int(input(f"THE {i+1} NUMBER IS:"))
                         res=res+num
+                        numbers.append(num)
                     print(res)
-                    c.write(f"[{now}],{s} chose Addition and the answer is: {res}\n")
+                    c.write(f"[{now}] {s} chose Addition with numbers  {numbers}  and the answer is: {res}\n")
                 elif n==2:
                     n1=int(input("How many numbers do u want to enter:"))
                     res=int(input("Please specify the initial number you want to subtract from(Another n (number you selected) numbers will be asked from you after this!):"))
+                    numbers.append(res)
                     for i in range(n1):
                         num=int(input(f"THE {i+1} NUMBER IS:"))
                         res=res-num
+                        numbers.append(num)
                     print(res)
-                    c.write(f"[{now}],{s} chose Subtraction and the answer is: {res}\n")
+                    c.write(f"[{now}] {s} chose Subtraction with numbers {numbers} and the answer is: {res}\n")
                 elif n==3:
                     n1=int(input("How many numbers do u want to enter:"))
                     res=int(input("Please specify the initial number you want to MULTIPLY(Another n (number you selected) numbers will be asked from you after this!):"))
-                    #num=int(input("Please specify the initial number you want to subtract from:"))
+                    numbers.append(res)
                     for i in range(n1):
                         num=int(input(f"THE {i+1} NUMBER IS:"))
                         res=res*num
+                        numbers.append(num)
                     print(res)
-                    c.write(f"[{now}],{s} chose Multiplication and the answer is: {res}\n")
+                    c.write(f"[{now}] {s} chose Multiplication with numbers {numbers} and the answer is: {res}\n")
                 elif n==4:
                     n1=int(input("How many numbers do u want to enter:"))
                     res=int(input("Please specify the initial number you want to DIVIDE from(Another n (number you selected) numbers will be asked from you after this!):"))
+                    numbers.append(res)
                     for i in range(n1):
                         num1=int(input(f"THE {i+1} NUMBER IS:"))
                         if num1==0:
                             print("Cannot enter zero! Zero won't be counted.")
                         else:
                             res=res/num1
+                        numbers.append(num)
                     print(res)
-                    c.write(f"[{now}],{s} chose Division and the answer is: {res}\n")
+                    c.write(f"[{now}] {s} chose Division and the answer is: {res}\n")
                 elif n==5:
                     print("This is view history")
                     c.write(f"[{now}],{s} visited the history!\n")
+                    c.flush()
                     with open ("history.txt","r") as history:
                         contents = history.read()
                         print(contents)
                 elif n==6:
                     print("Bye! Have a nice day!")
-                    c.write(f"[{now}],{s} chose to exit the program\n")
+                    c.write(f"[{now}] {s} chose to exit the program\n")
                     break
                 else:
                     print("Wrong input.")
