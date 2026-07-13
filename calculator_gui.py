6feb6fa885c02f2b7c79fca16151ca91c4b2915b
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-13 20:38:22 +0530

    Add working button with display insert

--- calculator_gui.py
+++ calculator_gui.py
@@ -2,7 +2,10 @@ import tkinter as tk
 window=tk.Tk()
 window.title("CALCULATOR")
 display=tk.Entry(window)
-one=tk.Button(window,text='1')
 display.pack()
+def click_one():
+    display.insert(tk.END, '1')
+one=tk.Button(window,text='1', command=click_one)
+
 one.pack()
 window.mainloop()
