f63d98121efacf3fda0f66fe91de9910bc64ca90
Lubaina Lokhandwala <lubaina7411bsbp@gmail.com>
2026-07-16 01:03:15 +0530

    WIP: equal_click skeleton

--- calculator_gui.py
+++ calculator_gui.py
@@ -4,6 +4,7 @@ window.title("CALCULATOR")
 display=tk.Entry(window)
 display.pack()
 first_number = None
+second_number=None
 operator = None
 def click(value):
     display.insert(tk.END, f"{value}")
@@ -17,22 +18,19 @@ six=tk.Button(window,text='6', command=lambda:click(6))
 seven=tk.Button(window,text='7', command=lambda:click(7))
 eight=tk.Button(window,text='8', command=lambda:click(8))
 nine=tk.Button(window,text='9', command=lambda:click(9))
-zero.pack()
-one.pack()
-two.pack()
-three.pack()
-four.pack()
-five.pack()
-six.pack()
-seven.pack()
-eight.pack()
-nine.pack()
+zero.pack(); one.pack(); two.pack(); three.pack();four.pack();five.pack()
+six.pack();seven.pack();eight.pack();nine.pack()
 def operator_click(op):
     global first_number
     first_number=display.get()
     display.delete(0,tk.END)
     global operator
     operator=op
+def equal_click():
+    global second_number
+    second_number=display.get()
+equal=tk.Button(window, text="=", command=lambda:equal_click("="))
 plus=tk.Button(window,text='+', command=lambda:operator_click ("+"))
 plus.pack()
+equal.pack()
 window.mainloop()
